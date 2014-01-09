6ff951c6c06de5d66a8eae12f4a23dc1c138b324
Drew Gottlieb <gottlieb.drew@gmail.com>
2014-01-09 16:16:47 -0500

    Added client user model, users collection.
    Room can find, remove user. Room updates user's collection if their dj
    property changes.

--- scripts/room_models.js
+++ scripts/room_models.js
@@ -1,21 +1,94 @@
 $(function() {
-  window.models = {};
+  var models = window.models = {};
 
-  window.models.Room = Backbone.Model.extend({
+  models.User = Backbone.Model.extend({
     defaults: {
-      anonymous_listeners: 0,
-      connected: false
+      admin: false,
+      dj: false
+    }
+  });
+
+  models.Users = Backbone.Collection.extend({
+    model: models.User,
+
+    initialize: function() {
+      this.on('add', this.userAdded, this);
+      this.on('remove', this.userRemoved, this);
     },
 
-    initialize: function(options) {
-      console.log('Initialized room:', options);
+    userAdded: function(user) {
+      user.collection = this;
+    },
+
+    userRemoved: function(user) {
+      if (user.collection === this)
+        user.collection = null;
+    },
+
+    getUser: function(username) {
+      return this.findWhere({ username: username });
+    },
+
+    removeByUsername: function(username) {
+      var user = this.getUser(username);
+      if (user) this.remove(user);
+      return user;
+    }
+  });
+
+  models.Room = Backbone.Model.extend({
+    defaults: {
+      anonymous_listeners: 0,
+      connected: false,
+      listeners: new models.Users(),
+      djs: new models.Users()
     },
 
     reset: function() {
-      this.set({
-        anonymous_listeners: 0,
-        connected: false
-      });
+      this.set(this.defaults);
+      this.get('listeners').reset();
+      this.get('djs').reset();
+    },
+
+    addUser: function(user) {
+      var djs = this.get('djs');
+      var listeners = this.get('listeners');
+
+      if (!(user instanceof models.User))
+        user = new models.User(user);
+
+      user.on('change:dj', this.userDjChanged, this);
+
+      if (user.get('dj'))
+        djs.add(user);
+      else
+        listeners.add(user);
+    },
+
+    removeUser: function(username) {
+      var dj = this.get('djs').removeByUsername(username);
+      var listener = this.get('listeners').removeByUsername(username);
+
+      var user = dj || listener;
+      if (user)
+        user.off('change:dj');
+    },
+
+    userDjChanged: function(user) {
+      var dj = user.get('dj');
+      if (dj) {
+        listeners.remove(user);
+        djs.add(user);
+      } else {
+        djs.remove(user);
+        listeners.add(user);
+      }
+    },
+
+    getUser: function(username) {
+      var djs = this.get('djs');
+      var listeners = this.get('listeners');
+      return djs.getUser(username) || listeners.getUser(username);
     }
   });
 });
