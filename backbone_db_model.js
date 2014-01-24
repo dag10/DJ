7e572771a39c1de539d9d23a6e71d60e16e3b7da
Drew Gottlieb <gottlieb.drew@gmail.com>
2014-01-24 14:24:00 -0500

    Created BackboneDBModel to encapsulate orm models.
    Started room model that extends it.

---
+++ backbone_db_model.js
@@ -0,0 +1,50 @@
+/* backbone_db_model.js
+ * A base Backbone model that encapsulates a db model.
+ */
+
+var _ = require('underscore');
+var Backbone = require('backbone');
+
+module.exports = Backbone.Model.extend({
+  defaults: {
+    autosave: true
+  },
+
+  initialize: function(entity) {
+    this.on('change:entity', this.entityChanged, this);
+    this.set({ entity: entity });
+  },
+
+  entity: function() {
+    return this.get('entity');
+  },
+
+  entityChanged: function() {
+    var entity = this.get('entity');
+
+    // Copy db model attributes to backbone model.
+    this.set(entity, { silent: true });
+    this.set({ model: entity.model });
+
+    // When one of these attributes is changed in the backbone model, update
+    // and save the db model if autosave is enabled.
+    this.off('change', this.save);
+    Object.keys(entity).forEach(_.bind(function(key) {
+      this.on('change:' + key, function() {
+        if (this.get('autosave'))
+          this.save();
+      }, this);
+    }, this));
+  },
+
+  sync: function(method, model) {
+    this.get('entity').save(_.bind(function(err) {
+      if (err) {
+        winston.error('Failed to save model: ' + this.get('model'));
+      } else {
+        winston.info('Saved model: ' + this.get('model')); // TODO delete
+      }
+    }, this));
+  }
+});
+
---
+++ room.js
@@ -0,0 +1,11 @@
+/* room.js
+ * Backbone model to define behavior for a room.
+ */
+
+var winston = require('winston');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var BackboneDBModel = require('./backbone_db_model');
+
+module.exports = BackboneDBModel.extend();
+
