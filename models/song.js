7358bf0f1ee75776abae09b5d6580e85ae21c134
Drew Gottlieb <gottlieb.drew@gmail.com>
2014-06-08 18:40:52 -0700

    Added source and uuid fields to song model.
    Adds default values for each existing song entity.

--- models/song.js
+++ models/song.js
@@ -3,11 +3,16 @@
  */
 
 var orm = require('orm');
+var migration_utils = require('../utils/migration_utils.js');
 
 var Song;
 
 exports.define = function(db, models) {
   Song = db.define('song', {
+    source: {
+      type: 'text', size: 20, required: true },
+    uuid: {
+      type: 'text', size: 40, required: true },
     title: {
       type: 'text', required: true },
     artist: {
@@ -19,6 +24,9 @@ exports.define = function(db, models) {
     timeUploaded: {
       type: 'date', required: true }
   }, {
+    validations: {
+      uuid: orm.enforce.unique({ ignoreCase: true })
+    },
     methods: {
       getLogName: function() {
         return this.title + ' (' + this.id + ')';
@@ -52,6 +60,25 @@ exports.associate = function(models) {
   });
   Song.hasOne('artwork', models.file, {
     autoFetch: true
+  });
+};
+
+exports.migrate = function(migrate, db) {
+  // Add source field and uuid fields.
+  // Source defaults to "upload" since that's all we've had up till this point.
+  // UUID defaults to the sha1 hash of "upload:<id>".
+  migrate('add source and uuid', function(next) {
+    migration_utils.runQueries([
+      'ALTER TABLE song ADD source VARCHAR(20) NOT NULL FIRST',
+      'ALTER TABLE song ADD uuid VARCHAR(40) NOT NULL AFTER source',
+      'UPDATE song SET source=\'upload\'',
+      'UPDATE song SET uuid=SHA1(CONCAT(source, \':\', id))'
+    ], db, 'song', next);
+  }, function(next) {
+    migration_utils.runQueries([
+      'ALTER TABLE song DROP source',
+      'ALTER TABLE song DROP uuid'
+    ], db, 'song', next);
   });
 };
 
